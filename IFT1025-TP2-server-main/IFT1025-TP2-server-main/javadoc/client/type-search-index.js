d8bf9da7ca7fc7e5ec15d67cb3f19c57fb56eda2
Zachh7 <zacharybouliane@gmail.com>
2023-04-14 17:48:46 -0400

    fini je pense

---
+++ IFT1025-TP2-server-main/IFT1025-TP2-server-main/javadoc/client/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"client_fx","l":"client_fx"},{"p":"client_fx","l":"Controleur"},{"p":"client_fx","l":"Modele"},{"p":"client_fx","l":"Vue"}];updateSearchResults();
--- IFT1025-TP2-server-main/IFT1025-TP2-server-main/src/main/java/client_fx/client_fx.java
+++ IFT1025-TP2-server-main/IFT1025-TP2-server-main/src/main/java/client_fx/client_fx.java
@@ -1,5 +1,8 @@
 package client_fx;
 
+/**
+ * Demarre le client en version affichage.
+ */
 public class client_fx {
     public static void main(String[] args) {
         Vue.main(args);
